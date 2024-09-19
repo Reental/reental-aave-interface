7d3fef5237a038553dcb395721f219a8fdd05baa
Javier Ortiz <javieroc@gmail.com>
2024-09-19 12:56:14 +0200

    feat(TopBarNotify): use .env var to show personalized info

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { ChainId } from '@aave/contract-helpers';
+// import { ChainId } from '@aave/contract-helpers';
 import { Box } from '@mui/material';
 import { useRouter } from 'next/router';
 import React, { ReactNode } from 'react';
@@ -14,109 +14,101 @@ import { AppFooter } from './AppFooter';
 import { AppHeader } from './AppHeader';
 import TopBarNotify from './TopBarNotify';
 
-const getCampaignConfigs = (
+const getCampaignConfigs = ({}: { openMarket: (market: CustomMarket) => void }) =>
   // openSwitch: (underlyingAsset: string) => void,
-  openMarket: (market: CustomMarket) => void
-) => ({
-  [ChainId.base]: {
-    notifyText: 'A new incentives campaign is live on the Base market',
-    buttonText: 'Explore Base',
-    buttonAction: {
-      type: 'route' as const,
-      value: '/markets/?marketName=proto_base_v3',
-    },
-    bannerVersion: 'base-incentives-v1',
-    icon: '/icons/networks/base.svg',
-  },
-
-  // [ChainId.sonic]: {
-  //   notifyText: 'Swaps are now live on Sonic',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.sonic),
-  //   },
-  //   bannerVersion: 'sonic-incentives-v1',
-  //   icon: '/icons/networks/sonic.svg',
-  // },
-
-  [ChainId.mainnet]: {
-    notifyText: 'The Plasma market is now live.',
-    buttonText: 'Get Started',
-    buttonAction: {
-      type: 'function' as const,
-      value: () => openMarket(CustomMarket.proto_plasma_v3),
-    },
-    bannerVersion: 'plasma-market-v0',
-    icon: '/icons/networks/plasma.svg',
-  },
-
-  // [ChainId.polygon]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.polygon),
-  //   },
-  //   bannerVersion: 'polygon-swap-v1',
-  //   icon: '/icons/networks/polygon.svg',
-  // },
-
-  // [ChainId.avalanche]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.avalanche),
-  //   },
-  //   bannerVersion: 'avalanche-swap-v1',
-  //   icon: '/icons/networks/avalanche.svg',
-  // },
-
-  // [ChainId.arbitrum_one]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.arbitrum_one),
-  //   },
-  //   bannerVersion: 'arbitrum-swap-v1',
-  //   icon: '/icons/networks/arbitrum.svg',
-  // },
-
-  // [ChainId.optimism]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.optimism),
-  //   },
-  //   bannerVersion: 'optimism-swap-v1',
-  //   icon: '/icons/networks/optimism.svg',
-  // },
-
-  // [ChainId.xdai]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.xdai),
-  //   },
-  //   bannerVersion: 'gnosis-swap-v1',
-  //   icon: '/icons/networks/gnosis.svg',
-  // },
-
-  // [ChainId.bnb]: {
-  //   notifyText: 'Swap tokens directly in the Aave App',
-  //   buttonText: 'Swap Now',
-  //   buttonAction: {
-  //     type: 'function' as const,
-  //     value: () => openSwitch('', ChainId.bnb),
-  //   },
-  //   bannerVersion: 'binance-swap-v1',
-  //   icon: '/icons/networks/binance.svg',
-  // },
-});
+  // openMarket: (market: CustomMarket) => void
+  ({
+    // [ChainId.base]: {
+    //   notifyText: 'A new incentives campaign is live on the Base market',
+    //   buttonText: 'Explore Base',
+    //   buttonAction: {
+    //     type: 'route' as const,
+    //     value: '/markets/?marketName=proto_base_v3',
+    //   },
+    //   bannerVersion: 'base-incentives-v1',
+    //   icon: '/icons/networks/base.svg',
+    // },
+    // [ChainId.sonic]: {
+    //   notifyText: 'Swaps are now live on Sonic',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.sonic),
+    //   },
+    //   bannerVersion: 'sonic-incentives-v1',
+    //   icon: '/icons/networks/sonic.svg',
+    // },
+    // [ChainId.mainnet]: {
+    //   notifyText: 'The Plasma market is now live.',
+    //   buttonText: 'Get Started',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openMarket(CustomMarket.proto_plasma_v3),
+    //   },
+    //   bannerVersion: 'plasma-market-v0',
+    //   icon: '/icons/networks/plasma.svg',
+    // },
+    // [ChainId.polygon]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.polygon),
+    //   },
+    //   bannerVersion: 'polygon-swap-v1',
+    //   icon: '/icons/networks/polygon.svg',
+    // },
+    // [ChainId.avalanche]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.avalanche),
+    //   },
+    //   bannerVersion: 'avalanche-swap-v1',
+    //   icon: '/icons/networks/avalanche.svg',
+    // },
+    // [ChainId.arbitrum_one]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.arbitrum_one),
+    //   },
+    //   bannerVersion: 'arbitrum-swap-v1',
+    //   icon: '/icons/networks/arbitrum.svg',
+    // },
+    // [ChainId.optimism]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.optimism),
+    //   },
+    //   bannerVersion: 'optimism-swap-v1',
+    //   icon: '/icons/networks/optimism.svg',
+    // },
+    // [ChainId.xdai]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.xdai),
+    //   },
+    //   bannerVersion: 'gnosis-swap-v1',
+    //   icon: '/icons/networks/gnosis.svg',
+    // },
+    // [ChainId.bnb]: {
+    //   notifyText: 'Swap tokens directly in the Aave App',
+    //   buttonText: 'Swap Now',
+    //   buttonAction: {
+    //     type: 'function' as const,
+    //     value: () => openSwitch('', ChainId.bnb),
+    //   },
+    //   bannerVersion: 'binance-swap-v1',
+    //   icon: '/icons/networks/binance.svg',
+    // },
+  });
 
 export function MainLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -127,7 +119,9 @@ export function MainLayout({ children }: { children: ReactNode }) {
     router.push(`/markets/?marketName=${market}`);
   };
 
-  const campaignConfigs = getCampaignConfigs(openMarket);
+  const campaignConfigs = getCampaignConfigs({ openMarket });
+
+  // const { visible = false, learnMoreLink, buttonText, notifyText, icon } = NOTIFY_BAR;
 
   return (
     <>
