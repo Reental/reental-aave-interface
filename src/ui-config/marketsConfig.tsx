f8e1a4edc71fa00988883382a62dbdba412f3684
Javier Ortiz <javieroc@gmail.com>
2024-11-06 12:09:06 +0100

    feat (polygon): added config

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -1,7 +1,7 @@
 import { ChainId } from '@aave/contract-helpers';
 import { ReactNode } from 'react';
 
-import { AaveV3Sepolia } from './custom';
+import { AaveV3Polygon, AaveV3Sepolia } from './custom';
 
 // Enable for premissioned market
 // import { PermissionView } from 'src/components/transactions/FlowCommons/PermissionView';
@@ -60,49 +60,31 @@ export const marketsData: {
   [key in keyof typeof CustomMarket]: MarketDataType;
 } = {
   [CustomMarket.reental_polygon_v3]: {
-    // marketTitle: 'Polygon',
-    // market: CustomMarket.reental_polygon_v3,
-    // chainId: ChainId.polygon,
-    // v3: true,
-    // enabledFeatures: {
-    //   liquiditySwap: true,
-    //   incentives: true,
-    //   collateralRepay: false,
-    //   debtSwitch: false,
-    //   withdrawAndSwitch: true,
-    //   switch: false,
-    // },
-    // subgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${apiKey}/subgraphs/id/Co2URyXjnxaw8WqxKyVHdirq9Ahhm5vcTs4dMedAq211`,
-    // addresses: {
-    //   LENDING_POOL_ADDRESS_PROVIDER: AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
-    //   LENDING_POOL: AaveV3Polygon.POOL,
-    //   WETH_GATEWAY: AaveV3Polygon.WETH_GATEWAY,
-    //   REPAY_WITH_COLLATERAL_ADAPTER: AaveV3Polygon.REPAY_WITH_COLLATERAL_ADAPTER,
-    //   SWAP_COLLATERAL_ADAPTER: AaveV3Polygon.SWAP_COLLATERAL_ADAPTER,
-    //   WALLET_BALANCE_PROVIDER: AaveV3Polygon.WALLET_BALANCE_PROVIDER,
-    //   UI_POOL_DATA_PROVIDER: AaveV3Polygon.UI_POOL_DATA_PROVIDER,
-    //   UI_INCENTIVE_DATA_PROVIDER: AaveV3Polygon.UI_INCENTIVE_DATA_PROVIDER,
-    //   COLLECTOR: AaveV3Polygon.COLLECTOR,
-    //   DEBT_SWITCH_ADAPTER: AaveV3Polygon.DEBT_SWAP_ADAPTER,
-    //   WITHDRAW_SWITCH_ADAPTER: AaveV3Polygon.WITHDRAW_SWAP_ADAPTER,
-    // },
     marketTitle: 'Reental',
     market: CustomMarket.reental_polygon_v3,
+    chainId: ChainId.polygon,
     v3: true,
-    chainId: ChainId.sepolia,
     enabledFeatures: {
-      faucet: true,
+      liquiditySwap: true,
+      incentives: true,
+      collateralRepay: false,
+      debtSwitch: false,
+      withdrawAndSwitch: true,
+      switch: false,
     },
+    // subgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${apiKey}/subgraphs/id/Co2URyXjnxaw8WqxKyVHdirq9Ahhm5vcTs4dMedAq211`,
     addresses: {
-      LENDING_POOL_ADDRESS_PROVIDER: AaveV3Sepolia.POOL_ADDRESSES_PROVIDER,
-      LENDING_POOL: AaveV3Sepolia.POOL,
-      WETH_GATEWAY: AaveV3Sepolia.WETH_GATEWAY,
-      FAUCET: AaveV3Sepolia.FAUCET,
-      WALLET_BALANCE_PROVIDER: AaveV3Sepolia.WALLET_BALANCE_PROVIDER,
-      UI_POOL_DATA_PROVIDER: AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
-      UI_INCENTIVE_DATA_PROVIDER: AaveV3Sepolia.UI_INCENTIVE_DATA_PROVIDER,
-      GHO_TOKEN_ADDRESS: '0xc4bF5CbDaBE595361438F8c6a187bDc330539c60',
-      GHO_UI_DATA_PROVIDER: '0x69B9843A16a6E9933125EBD97659BA3CCbE2Ef8A',
+      LENDING_POOL_ADDRESS_PROVIDER: AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
+      LENDING_POOL: AaveV3Polygon.POOL,
+      WETH_GATEWAY: AaveV3Polygon.WETH_GATEWAY,
+      REPAY_WITH_COLLATERAL_ADAPTER: AaveV3Polygon.REPAY_WITH_COLLATERAL_ADAPTER,
+      SWAP_COLLATERAL_ADAPTER: AaveV3Polygon.SWAP_COLLATERAL_ADAPTER,
+      WALLET_BALANCE_PROVIDER: AaveV3Polygon.WALLET_BALANCE_PROVIDER,
+      UI_POOL_DATA_PROVIDER: AaveV3Polygon.UI_POOL_DATA_PROVIDER,
+      UI_INCENTIVE_DATA_PROVIDER: AaveV3Polygon.UI_INCENTIVE_DATA_PROVIDER,
+      COLLECTOR: AaveV3Polygon.COLLECTOR,
+      DEBT_SWITCH_ADAPTER: AaveV3Polygon.DEBT_SWAP_ADAPTER,
+      WITHDRAW_SWITCH_ADAPTER: AaveV3Polygon.WITHDRAW_SWAP_ADAPTER,
     },
   },
   [CustomMarket.reental_sepolia_v3]: {
