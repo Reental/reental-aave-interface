3247ea2808c383b347da5783daf0d3c79c3e1eb1
Javier Ortiz <javieroc@gmail.com>
2024-11-20 12:40:32 +0100

    feat(seo) update seo info

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -130,11 +130,11 @@ export default function MyApp(props: MyAppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Meta
-        title={'Open Source Liquidity Protocol'}
+        title={'Collateralization platform'}
         description={
-          'Aave is an Open Source Protocol to create Non-Custodial Liquidity Markets to earn interest on supplying and borrowing assets with a variable or stable interest rate. The protocol is designed for easy integration into your products and services.'
+          "Maximize the returns on your real estate investment in Reental using the world's most popular decentralized lending platform."
         }
-        imageUrl="https://app.aave.com/aave-com-opengraph.png"
+        imageUrl="https://d37o222i0yqhd1.cloudfront.net/images/reenlever_seo.png"
       />
       <LanguageProvider>
         <QueryClientProvider client={queryClient}>
