f86c0cd17366fbb827588c5c4307539ea876cb79
Javier Ortiz <javieroc@gmail.com>
2024-11-20 12:40:32 +0100

    feat(seo) update seo info

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -9,19 +9,16 @@ type MetaProps = {
 };
 
 export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
-  const formattedTitle = `Aave - ${title}`;
   return (
     <Head>
-      <title>{formattedTitle}</title>{' '}
+      <title>Reenlever - {title}</title>
       <meta name="description" content={description} key="description" />
-      <meta property="og:title" content={`Aave - ${title}`} key="title" />
+      <meta property="og:title" content={`Reenlever - ${title}`} key="title" />
       <meta property="og:description" content={description} key="ogdescription" />
       {imageUrl && <meta property="og:image" content={imageUrl} key="ogimage" />}
       {imageUrl && <meta name="twitter:image" content={imageUrl} key="twitterimage" />}
-      {imageUrl && (
-        <meta name="twitter:image:alt" content={`aave governance image`} key="twitteralt" />
-      )}
-      <meta name="twitter:site" content="@AaveAave" key="twittersite" />
+      {imageUrl && <meta name="twitter:image:alt" content={`Reenlever image`} key="twitteralt" />}
+      <meta name="twitter:site" content="@Reental_co" key="twittersite" />
       <meta
         property="twitter:card"
         content={imageUrl ? 'summary_large_image' : 'summary'}
@@ -30,14 +27,8 @@ export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
       <meta name="twitter:title" content={title} key="twittertitle" />
       <meta name="twitter:description" content={description} key="twitterdescription" />
       {timestamp && <meta name="revised" content={timestamp} key="timestamp" />}
-      <meta
-        name="keywords"
-        key="keywords"
-        content="Decentralized Finance, DeFi, lending, borrowing, stablecoins, Ethereum, assets, erc-20, smart contracts, open finance, trustless"
-      />
-      <link rel="apple-touch-icon" href="/aave_180.png" />
-      <meta name="apple-mobile-web-app-title" content={`Aave`} />
-      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content={`Reenlever`} />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     </Head>
   );
