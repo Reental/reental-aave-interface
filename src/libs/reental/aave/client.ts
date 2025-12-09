47b8d9c578d59198901a867efc9fb24e75e12acd
victormer <victormerinokosina@protonmail.com>
2025-12-09 12:53:51 +0100

    chore: update alchemy subgraph (goldsky) and ignore env

--- src/libs/reental/aave/client.ts
+++ src/libs/reental/aave/client.ts
@@ -3,8 +3,8 @@ import { GraphQLClient } from 'graphql-request';
 
 const endpoints = {
   11155111:
-    'https://subgraph.satsuma-prod.com/c18b322af44b/reental--3960108/reental-aave-sepolia/api',
-  137: 'https://subgraph.satsuma-prod.com/c18b322af44b/reental--3960108/reental-aave-polygon/api',
+    'https://api.goldsky.com/api/public/project_cmgziws0r00205np2estl7o0e/subgraphs/reental-aave-sepolia/v0.0.1/gn',
+  137: 'https://api.goldsky.com/api/public/project_cmgziws0r00205np2estl7o0e/subgraphs/reental-aave-polygon/v0.0.1/gn',
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
