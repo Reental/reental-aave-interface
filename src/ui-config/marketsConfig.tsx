bab49ef15f7145d025974cdfe02c06dd19c489c4
Jesus Duran <duranbecool@gmail.com>
2026-08-06 10:18:38 -0400

    fix: logo

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -63,7 +63,7 @@ export const marketsData: {
     marketTitle: 'Reental Polygon',
     market: CustomMarket.reental_polygon_v3,
     chainId: ChainId.polygon,
-    logo: '/icons/tokens/reental.svg',
+    logo: '/icons/markets/reental.png',
     v3: true,
     enabledFeatures: {
       liquiditySwap: false,
@@ -93,7 +93,7 @@ export const marketsData: {
     market: CustomMarket.reental_sepolia_v3,
     v3: true,
     chainId: ChainId.sepolia,
-    logo: '/icons/tokens/reental.svg',
+    logo: '/icons/markets/reental.png',
     enabledFeatures: {
       faucet: true,
     },
